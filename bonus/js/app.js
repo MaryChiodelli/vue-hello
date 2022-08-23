38f9bc58012caac62517945c256abeec1ef2da53
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-23 15:30:12 +0200

    add image

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -1,6 +1,8 @@
 const app = new Vue({
     el: '#root',
     data: {
-        title: 'Hello Vue!'
+        title: 'Hello Vue!',
+        src: 'https://picsum.photos/id/101/300/200',
+        alt: 'building'
     }
 });
